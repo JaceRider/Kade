d2bc251cd26b2007b6bd4bde6d93269333d78646
jacerider <jacerider@gmail.com>
2015-02-23 19:00:59 -0600

    Another attempt at Heroku success.

--- tasks/register/prod.js
+++ tasks/register/prod.js
@@ -1,4 +1,11 @@
 module.exports = function (grunt) {
 	// Moved into heroku.js
-	grunt.registerTask('prod', []);
+	grunt.registerTask('prod', [
+		'sails-linker:prodJs',
+		'sails-linker:prodStyles',
+		'sails-linker:devTpl',
+		'sails-linker:prodJsJade',
+		'sails-linker:prodStylesJade',
+		'sails-linker:devTplJade'
+	]);
 };
